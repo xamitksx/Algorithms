c6e6b89cef56fdef8deb82d0c7fc62ec6b32b283
AKS <pracaks2017@gmail.com>
2017-07-22 00:10:08 +0530

    Update HISTOGRA(Largest Rectangle in a Histogram).cpp

--- HISTOGRA(Largest Rectangle in a Histogram).cpp
+++ HISTOGRA(Largest Rectangle in a Histogram).cpp
@@ -11,7 +11,6 @@ using namespace std;
 
 long long HT[SZ];
 int WIDTH[SZ];
-long long max_size = 0;
 
 int main()
 {
@@ -24,7 +23,7 @@ int main()
 		cin>>noOfItem;
 		if(noOfItem==0)
 			break;
-		max_size = 0;
+		long long max_area = 0;
 		HT[0]=0;
 		WIDTH[0]=0;
 		int top=0;
@@ -39,22 +38,21 @@ int main()
 			}
 			else
 			{
-				//int new_ht =  0;
-				int new_wd = 0; 
+				int TotalPoppedWidth = 0; 
 				///NEED to POP out from ARRAY till we get no <=ht
 				while(HT[top] > ht)
 				{
-					
-					new_wd = new_wd + WIDTH[top];
-					long long new_size = HT[top] *  new_wd ;
-					if( new_size > max_size )
-						max_size = new_size ;
+			 		//Stack will always have heights in Non Decreasing order.. 
+					TotalPoppedWidth = TotalPoppedWidth + WIDTH[top];
+					long long area = HT[top] *  TotalPoppedWidth ;
+					if( area > max_area )
+						max_area = area ;
 					///POP
 					top--;
 				}
 				////PUSH////
 				HT[++top] = ht;
-				WIDTH[top] = new_wd + 1;
+				WIDTH[top] = TotalPoppedWidth + 1;  ///TotalPoppedWidth  and width of current Bar.
 			}//else
 		}//while
 		///////AT end POP all and update maxsize;
@@ -62,14 +60,14 @@ int main()
 		while(top!=0)
 		{
 			wd = wd + WIDTH[top];
-			long long size = HT[top] *  wd ;
-			if( size > max_size )
-				max_size = size ;
+			long long area = HT[top] *  wd ;
+			if( area > max_area )
+				max_area = area ;
 			///POP
 					top--;
 		}
 		////RESULT
-		cout<<max_size<<endl;
+		cout<<max_area<<endl;
 	}//while infinite
 	return 0;
 }
