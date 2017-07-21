0539b0a0fa6435f352066e889275a899526340d4
AKS <pracaks2017@gmail.com>
2017-07-22 00:31:06 +0530

    Update Dijkstra_PriorityQueue.cpp

--- Dijkstra_PriorityQueue.cpp
+++ Dijkstra_PriorityQueue.cpp
@@ -1,7 +1,7 @@
 #if 1
 /*
 Problem : http://www.spoj.com/problems/BENEFACT/
-To find Longest path convert all weights as negative and find shortest path with Dijkastra. shortest in -ve will be Longest in +ve 
+To find Longest path convert all weights as negative and find shortest path with Dijkastra. shortest in -ve will be Longest in +ve
 apply Dijkastra  for all nodes which have Degree 1 leaf node.
 ---
 DataStructure Used Priority Queue
@@ -13,9 +13,9 @@ using namespace std;
 #define VERTEX 50000
 #define INT_MAX 0x7FFFFFFF
 #define MIN(a,b)  ((a<b)?a:b)
-typedef struct{
+typedef struct {
 	int v;
-	long c;
+	long d;
 }__NODE;
 int NEXT = 0;
 class PQ
@@ -27,15 +27,15 @@ private:
 
 	int LEFT(int k) { return 2 * k; }
 	int RIGHT(int k) { return 2 * k + 1; }
-	int PARENT(int k){ return k / 2; }
+	int PARENT(int k) { return k / 2; }
 	void sink(int k);
 	void swim(int k);
 	void SWAPI(int a, int b);
-	bool IsLess(int a, int b){ return (heap[a].c < heap[b].c) ? true : false; }
-	bool IsMore(int a, int b){ return (heap[a].c > heap[b].c) ? true : false; }
+	bool IsLess(int a, int b) { return (heap[a].d < heap[b].d) ? true : false; }
+	bool IsMore(int a, int b) { return (heap[a].d > heap[b].d) ? true : false; }
 
 public:
-	PQ() :size(0), maxSize(0){};
+	PQ() :size(0), maxSize(0) {};
 	int maxSize;
 	void decreaseKey(int vertex, int val);
 	void InsertKey(int val, int vertex);
@@ -48,12 +48,12 @@ public:
 
 class PQ pq;
 typedef vector < pair<int, long> > _LIST;
-long dijkastra(int source ,int N, vector < _LIST > &aList)
+long dijkastra(int source, int N, vector < _LIST > &aList)
 {
 	long dist = 0;;
 	pq.maxSize = N;
 	bool done[VERTEX + 1] = { false };
-	
+
 	for (int i = 1; i <= N; i++)
 	{
 		pq.InsertKey(i, INT_MAX);
@@ -62,23 +62,23 @@ long dijkastra(int source ,int N, vector < _LIST > &aList)
 	pq.decreaseKey(source, 0);  // source = 0
 	while (pq.IsEmpty() == false)
 	{
-		__NODE U = pq.delTOP();  //EXTRACT- MIN from Priority QUEUE		
-		if (dist > U.c)
+		__NODE vertex = pq.delTOP();  //EXTRACT- MIN from Priority QUEUE		
+		if (dist > vertex.d)
 		{
-			dist = U.c;
-			NEXT = U.v;
+			dist = vertex.d;
+			NEXT = vertex.v;
 		}
-		done[U.v] = true; //set min_v to done 
-		////RELAXATION
-		for (int i = 0; i < (int)aList[U.v].size(); i++)
+		done[vertex.v] = true; //set min_v to done 
+						  ////RELAXATION
+		for (int i = 0; i < (int)aList[vertex.v].size(); i++)
 		{
-			int u = U.v;
-			int v = aList[U.v][i].first;
-			long Euv = aList[U.v][i].second;
-			if (done[v] == false && U.c + Euv < pq.getkey(v))
+			int u = vertex.v;
+			int v = aList[u][i].first;
+			long distuv = aList[u][i].second;
+			if (done[v] == false && vertex.d + distuv < pq.getkey(v))
 			{
 				//update dist 
-				pq.decreaseKey(v, U.c + Euv);
+				pq.decreaseKey(v, vertex.d + distuv);
 			}
 		}
 	}
@@ -88,7 +88,7 @@ long dijkastra(int source ,int N, vector < _LIST > &aList)
 int main()
 {
 	int T;
-//	freopen("BENEFACT_input.txt","r",stdin);
+	//	freopen("BENEFACT_input.txt","r",stdin);
 	cin >> T;
 	while (T--)
 	{
@@ -98,11 +98,11 @@ int main()
 		vector <_LIST> adjList(N + 1);
 		for (int i = 1; i <= E; i++)
 		{
-			int u, v, c;
-			cin >> u >> v >> c;
-			c = c *(-1);
-			adjList[u].push_back(make_pair(v, c));
-			adjList[v].push_back(make_pair(u, c)); //undirected Graph
+			int u, v, d;
+			cin >> u >> v >> d;
+			d = d *(-1);
+			adjList[u].push_back(make_pair(v, d));
+			adjList[v].push_back(make_pair(u, d)); //undirected Graph
 		}
 		// apply Dijkastra  for all nodes which have Degree 1 
 		long res = INT_MAX;
@@ -125,7 +125,7 @@ int main()
 /* Get Key(distance ) value from Heap of Vertex V*/
 long PQ::getkey(int v)
 {
-	return heap[index[v]].c;
+	return heap[index[v]].d;
 }
 void PQ::reset(void)
 {
@@ -137,7 +137,7 @@ void PQ::reset(void)
 	maxSize = 0;
 }
 /*Swap Two Elements of given indexes Also updateds the index Array*/
-void PQ::SWAPI(int a, int b){
+void PQ::SWAPI(int a, int b) {
 	__NODE t = heap[a]; heap[a] = heap[b]; heap[b] = t;
 	index[heap[a].v] = a;
 	index[heap[b].v] = b;
@@ -145,7 +145,7 @@ void PQ::SWAPI(int a, int b){
 /*Inreases size of Heap and Insert element at Last Index and Maintains the MIN condition of HEAP by SWIM*/
 void PQ::InsertKey(int vertex, int val)
 {
-	heap[++size].c = val; //Increase Last Index and Insert Element there 
+	heap[++size].d = val; //Increase Last Index and Insert Element there 
 	heap[size].v = vertex;
 	index[vertex] = size;
 	swim(size); // Heapify from Last element Bottom UP
@@ -154,7 +154,7 @@ void PQ::InsertKey(int vertex, int val)
 void PQ::decreaseKey(int vertex, int val)
 {
 	int v = index[vertex]; //get Heap Index "v" for given vertex 
-	heap[v].c = val;  //update value
+	heap[v].d = val;  //update value
 	swim(v);  // Heapify 
 }
 /* BOTTOM up HEAPIFY  pushes Larger no to TOP  */
